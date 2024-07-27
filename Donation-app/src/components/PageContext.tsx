3d95eae5f0dcab7dd6c82bb8ff12ed4adcacc8dc
AJasem <ajassem.ahmad@gmail.com>
2024-07-27 20:22:01 +0200

    card info page

---
+++ Donation-app/src/components/PageContext.tsx
@@ -0,0 +1,34 @@
+import React, { createContext, useContext, useState, ReactNode } from 'react';
+
+interface PageContextProps {
+  page: number;
+  setPage: (page: number) => void;
+  selectedAmount: number | null;
+  setSelectedAmount: (amount: number | null) => void;
+  error: string | null;
+  setError: (error: string | null) => void;
+}
+
+const PageContext = createContext<PageContextProps | undefined>(undefined);
+
+export const PageProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
+  const [page, setPage] = useState<number>(0);
+  const [selectedAmount, setSelectedAmount] = useState<number | null>(null);
+  const [error, setError] = useState<string | null>(null)
+
+  return (
+    <PageContext.Provider value={{ page, setPage, selectedAmount, setSelectedAmount, error, setError}}>
+      {children}
+    </PageContext.Provider>
+  );
+};
+
+export const usePage = (): PageContextProps => {
+  const context = useContext(PageContext);
+  if (!context) {
+    throw new Error('usePage must be used within a PageProvider');
+  }
+  return context;
+};
+
+export default PageContext;
